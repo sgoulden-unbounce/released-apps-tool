9f194990c5ae9068115b11de8ba5ed5c68930eca
Sam Goulden <sam.goulden@unbounce.com>
2022-08-22 08:55:34 -0700

    Cleanup

--- deps.ts
+++
@@ -1,3 +0,0 @@
-import Checkbox from "https://esm.sh/@material-ui/core@4.12.4/Checkbox";
-
-export { Checkbox };
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,15 +5,13 @@ import { tw } from "../utils/twind.ts";
 
 export default function Home() {
   return (
-    <div>
-      <img
-        src="/unbounce-icon-dark.svg"
-        class={tw`h-24`}
-      />
-      <h3 class={tw`text-xl`}>
-        App Release tool
-      </h3>
-      <App />
-    </div>
+    <html>
+      <title>Released Apps Tool</title>
+      <div>
+        <img src="/unbounce-icon-dark.svg" class={tw`h-24`} />
+        <h3 class={tw`text-xl`}>App Release tool</h3>
+        <App />
+      </div>
+    </html>
   );
 }
